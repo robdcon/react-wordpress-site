c8ae6a1a62c317774c1ad9a041613b6121b74d57
robdcon <robdcon@gmail.com>
2020-03-13 13:31:43 +0000

    adds posts component

---
+++ src/Posts/Posts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+//import { Test } from './Posts.styles';
+
+const Posts = (props) => {
+  // Set Posts Hook
+  const [posts, setPosts] = usestate([]);
+  <div className="PostsWrapper">
+    Test content
+  </div>
+};
+
+Posts.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Posts.defaultProps = {
+  // bla: 'test',
+};
+
+export default Posts;
